86ff7fda25137640e5ae2008cbdb57dc3f48d749
Kijeosowo <pelumiwilliams23@gmail.com>
2026-03-11 07:11:24 +0100

    debugging

---
+++ app/services/bankApi.ts
@@ -0,0 +1,25 @@
+export type Bank = {
+  name: string;
+  slug: string;
+};
+const BASE_URL = "https://supermx1.github.io/nigerian-banks-api/data.json"
+// const BASE_URL = "https://api.nigerianbanklogos.xyz";
+
+/* GET ALL BANKS */
+export const getBanks = async (): Promise<Bank[]> => {
+  try {
+    const response = await fetch(BASE_URL);
+
+    const result = await response.json();
+
+    return result.data; // API returns { data: [] }
+  } catch (error) {
+    console.log("Error fetching banks:", error);
+    return [];
+  }
+};
+
+/* GET BANK LOGO */
+export const getBankLogo = (slug: string) => {
+  return `${BASE_URL}/logo/${slug}`;
+};
